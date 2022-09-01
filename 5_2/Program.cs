d00f0b62126df68ce538935cb0da537bc93aeb03
Victoria R <vik-rozanova@yandex.ru>
2022-09-02 01:52:41 +0300

    Add 5_2 sum of (not even positions) of array

---
+++ 5_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void SumNotEven()
+{
+    int[] Array = new int[new Random().Next(2, 15)];
+    int length = Array.Length;
+    int sum = 0;
+    
+    for (int index = 0; index < length; index++)
+    {
+        Array[index] = new Random().Next(-10, 11);
+        if(index % 2 == 1)
+        {
+            sum = sum + Array[index];
+        }
+    }
+    Console.WriteLine($"Первоначальный массив: [{String.Join(", ", Array)}]");
+    Console.WriteLine($"Сумма чисел нечётных ппозиций массива: {sum}");
+}
+SumNotEven();

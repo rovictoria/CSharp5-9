27206c2dae0860e5ee7bab18fda7789caa81ed4c
Victoria R <vik-rozanova@yandex.ru>
2022-09-02 01:23:42 +0300

    Add 5 star

--- 5_star/Program.cs
+++ 5_star/Program.cs
@@ -10,39 +10,51 @@
 
 void NewMas()
 {
-    int[] MyArr = new int[new Random().Next(2,11)];
-    int maxindex1 = MyArr.Length-1;
+    int[] MyArr = new int[new Random().Next(2, 11)];
+    int maxindex1 = MyArr.Length - 1;
 
-    int[] NewArr = new int[((maxindex1 + 1) / 2) + ((maxindex1 + 1) % 2)] ;
-    int maxindex2 = NewArr.Length-1;
+    int[] NewArr = new int[((maxindex1 + 1) / 2) + ((maxindex1 + 1) % 2)];
+    int maxindex2 = NewArr.Length - 1;
 
-    for(int index = 0; index <= maxindex1; index++)
+    for (int index = 0; index <= maxindex1; index++)  //заполнила первоначальный
     {
-        MyArr[index] = new Random().Next(1,10);
+        MyArr[index] = new Random().Next(1, 10);
     }
 
     Console.WriteLine($"Первоначальный массив: [{String.Join(", ", MyArr)}]");
-    Console.Write($"Полученный массив [");
 
-    if()
-    for (int i = 0; i < ((maxindex1 + 2)/ 2 ); i++)
+    if (maxindex1 == 1)
     {
-        NewArr[i] = MyArr[i] * MyArr[maxindex1 - i];
-        Console.Write($"{NewArr[i]}, ");
-    }
-
-    if (maxindex2 % 2 == 0)
-    {
-        NewArr[maxindex2] = MyArr[maxindex1 / 2 ];
-        Console.Write($"{NewArr[maxindex2]}]");
+        Console.Write($"После умножения получим не массив (набор значений), а число. Не удовлетворяет условию :(");
     }
     else
     {
-        NewArr[maxindex2] = MyArr[maxindex1 / 2 ] * MyArr[(maxindex1 / 2) + 1];
-        Console.Write($"{NewArr[maxindex2]}]");
+        Console.Write($"Полученный массив [");
+
+         if (maxindex1 % 2 == 0)       // то есть знач-е длины массива нечётное: 3, 5
+         {
+             for (int i = 0; i <= ((maxindex1 - 1) / 2); i++)     // если 3, то  до 1
+             {
+                NewArr[i] = MyArr[i] * MyArr[maxindex1 - i];
+                Console.Write($"{NewArr[i]}, ");
+             }
+             NewArr[maxindex2] = MyArr[maxindex1 / 2];  //и есть то среднее число, для него без зпт
+             Console.Write($"{NewArr[maxindex2]}]");
+         }
+
+
+         if (maxindex1 % 2 != 0)
+         {
+             for (int i = 0; i <= ((maxindex1 - 2) / 2); i++)
+             {
+                NewArr[i] = MyArr[i] * MyArr[maxindex1 - i];
+                Console.Write($"{NewArr[i]}, ");
+             }
+             NewArr[maxindex2] = MyArr[maxindex1 / 2] * MyArr[(maxindex1 / 2) + 1];
+             Console.Write($"{NewArr[maxindex2]}]");
+         }
+
     }
-
-
 }
 
 NewMas();

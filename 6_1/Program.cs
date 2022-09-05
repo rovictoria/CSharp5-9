b6e76f68495d32a205bd6ffceaa1e29ad4cebc75
Victoria R <vik-rozanova@yandex.ru>
2022-09-05 21:47:14 +0300

    Array[M] count nembers > 0 add

---
+++ 6_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+void MyArray()
+{
+    Console.Write("Задайте количество чисел, которое хотите ввести: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    if (M > 1)
+    {
+        int count = 0;
+
+        int[] arr = new int[M];
+
+        for (int i = 0; i < M; i++)
+        {
+            Console.Write("Введите число: ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            if (arr[i] > 0)
+            {
+                count++;
+            }
+        }
+        
+        Console.WriteLine($"Заданный массив: [{String.Join(", ", arr)}]");
+        Console.WriteLine($"Длина массива: {M}.");
+        Console.WriteLine($"Элементов, больших нуля: {count}.");
+    }
+    else 
+    {
+        Console.WriteLine("Вы не можете создать массив такой размерности, попробуйте ещё раз :)");
+    }
+}
+
+MyArray();

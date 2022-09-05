64d3f77ec5f33370f3b3d67337bb562c543a1011
Victoria R <vik-rozanova@yandex.ru>
2022-09-05 22:50:03 +0300

    comment double

--- 6_2/Program.cs
+++ 6_2/Program.cs
@@ -7,22 +7,23 @@
 void Fun()
 {
     Console.Write("Задайте k1: ");
-    int k1 = Convert.ToInt32(Console.ReadLine());
+    double k1 = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Задайте b1: ");
-    int b1 = Convert.ToInt32(Console.ReadLine());
+    double b1 = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Задайте k2: ");
-    int k2 = Convert.ToInt32(Console.ReadLine());
+    double k2 = Convert.ToDouble(Console.ReadLine());
 
     Console.Write("Задайте b2: ");
-    int b2 = Convert.ToInt32(Console.ReadLine());
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    
+    Console.WriteLine ($"Первое уравнение: y = {k1} * x + ({b1})");
+    Console.WriteLine($"Второе уравнение: y = {k2} * x + ({b2})");
 
-Console.WriteLine ($"Первое уравнение: y = {k1} * x + ({b1})");
-Console.WriteLine($"Второе уравнение: y = {k2} * x + ({b2})");
-// y = {k1}*x + b1; y = {k2}*x + b2
-// (b2-b1)= k1*x-k2*x
-// x = (b2-b1)/(k1-k2)
+    // y = {k1}*x + b1; y = {k2}*x + b2
+    // (b2-b1)= k1*x-k2*x
+    // x = (b2-b1)/(k1-k2)
 
     if (k1 != k2)
     {
@@ -30,10 +31,12 @@ Console.WriteLine($"Второе уравнение: y = {k2} * x + ({b2})");
         double y = k1 * x + b1;
         Console.WriteLine($"Точка пересечения прямых ({x} , {y})");
     }
+    
     else if (k1 == k2 && b1 != b2)
     {
         Console.WriteLine($"Прямые параллельны, точек пересечения нет");
     }
+    
     else if (k1 == k2 && b1 == b2)
     {
         Console.WriteLine($"Прямые cовпадают");

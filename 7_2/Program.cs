6b187f898b012a0f3e9e16cb296bd995fdbe3dd5
Victoria R <vik-rozanova@yandex.ru>
2022-09-07 15:22:50 +0300

    Find number's positions in array

---
+++ 7_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumberInArray(int[,] matr)
+{
+    Console.WriteLine("Введите число, которое хотите найти: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    string result = String.Empty;
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] == number)
+            {
+                result = $"{number} находится на позиции [{i}, {j}]";
+                Console.WriteLine(result);
+            }
+        }
+    }
+    if (result == String.Empty) Console.WriteLine($"Числа {number} нет в данном массиве.");
+}
+
+    int[,] matrix = new int[3, 4];
+    FillArray(matrix);
+    FindNumberInArray(matrix);
+
+// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.

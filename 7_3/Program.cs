dcbdbbfdbd96291b1c728c5986e8086459aa7166
Victoria R <vik-rozanova@yandex.ru>
2022-09-07 15:53:10 +0300

    7_3 each average of array's column

--- 7_3/Program.cs
+++ 7_3/Program.cs
@@ -6,3 +6,37 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 11);
+            Console.Write($"{matr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageValue(int[,] matr)
+{
+    double result;
+    double sum; 
+
+    for (int j = 0; j < matr.GetLength(1); j++) //4
+    {
+        sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++) //3
+        {
+            sum = sum + matr[i, j];
+        }
+        result = sum / matr.GetLength(0);
+        Console.WriteLine($"Среднее арифм. {j + 1} столбца = {Math.Round(result, 2)}");
+    }
+}
+
+int[,] matrix = new int[4, 4];
+FillArray(matrix);
+AverageValue(matrix);

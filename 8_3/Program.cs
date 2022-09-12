6a6602e1e60614e89c5b909db7c7e0f11c8651b4
Victoria R <vik-rozanova@yandex.ru>
2022-09-12 21:24:27 +0300

    8_3 add

--- 8_3/Program.cs
+++ 8_3/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void FillArrayRandom(int[,] matr)
+{
+    Console.WriteLine("Заданная матрица:");
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10, 11);
+            Console.Write($"{matr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Нахождение произведения двух матриц");
+
+void NewMatrix(int[,] matr1, int[,] matr2, int[,] matr3)
+{
+    Console.WriteLine("Результирующая матрица:");
+    for (int i = 0; i < matr3.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr3.GetLength(1); j++)
+        {
+            matr3[i, j] = matr1[i, j] * matr2[i, j];
+            Console.Write($"{matr3[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 2; //ввожу внутри кода такие моменты, чтобы избежать доп.условий при вводе пользователем
+int n = 3;
+
+int[,] matrix1 = new int[m, n];
+int[,] matrix2 = new int[m, n];
+int[,] matrix3 = new int[m, n];
+
+FillArrayRandom(matrix1);
+Console.WriteLine();
+FillArrayRandom(matrix2);
+Console.WriteLine();
+NewMatrix(matrix1, matrix2, matrix3);
